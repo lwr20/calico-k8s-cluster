555b192a3613071ae5ed5769166244ff87ecd1e4
Casey Davenport <casey@tigera.io>
2016-08-05 13:09:40 -0700

    Updates to Makefile, scripts, Vagrantfile.

--- vagrant/scripts/wait_for_cluster.py
+++ vagrant/scripts/wait_for_cluster.py
@@ -2,21 +2,21 @@ import time
 import json
 import sys
 import os
-from subprocess import check_output 
+from subprocess import check_output
 
 # Max time to wait for nodes to start.
 MAX_SECONDS = 60
 
 # Total number of nodes, including the master.
-NUM_NODES = 3
+NUM_NODES = 2
 
-kubectl_path = "%s/kubectl" % os.getcwd()
+kubectl_path = "kubectl"
 
 print("Waiting up to %ss for nodes..." % MAX_SECONDS)
 for i in range(MAX_SECONDS):
 	nodes = check_output([kubectl_path, "get", "nodes", "--output=json"])
 	nodes = json.loads(nodes)["items"]
-	
+
 	# Check if we should exit.
 	print("%s node(s) started" % len(nodes))
 	if len(nodes) >= NUM_NODES:
