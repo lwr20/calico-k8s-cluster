74629d51104f2fe9edd170fe84a89b2a25e6b99c
Lance Robson <lance@tigera.io>
2016-09-01 19:42:56 +0100

    Initial changes for k8s 1.3

--- gce/policy-scale-test/start.sh
+++ gce/policy-scale-test/start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Create namespaces.
-kubectl create -f ../build/manifests/namespaces/
-
 # Create DNS.
 kubectl create -f ../build/manifests/addons/dns-addon.yaml
-
-# Ensure isolation is on.
-echo "Enabling isolation on default namespace"
-kubectl annotate ns default "net.alpha.kubernetes.io/network-isolation=yes" --overwrite=true
 
 # Start the nginx ReplicationController and Service
 echo "Starting nginx Service"
